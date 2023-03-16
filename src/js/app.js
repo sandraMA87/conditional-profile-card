6ff884173027427a2475f3df64801609c43d6d0e
sandraMA87 <sandra.moas@hotmail.com>
2023-03-16 09:28:47 +0000

    profilecard

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,24 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || " Name "} ${variables.lastname ||
+    "Lastname "}</h1>          
+          <h2>${variables.role || " Role "} </h2>
+          <h3>${variables.city || "city"}, ${variables.country ||
+    "Country"}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/whereisandra_"${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/sandraMA87"${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/alesanchezr"${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/whereisandra_"${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
